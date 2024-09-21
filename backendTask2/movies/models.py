124c225a6843bbe80727a9c3d587c820c05e852a
Harshit Jangir <it.s.me.of.em@gmail.com>
2024-09-22 00:23:38 +0530

    Added Frontend.

--- backendTask2/movies/models.py
+++ backendTask2/movies/models.py
@@ -1,7 +1,16 @@
 from django.db import models
 
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
+    genre = models.CharField(max_length=50, blank=True, null=True)
     description = models.TextField()
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    release_year = models.IntegerField(blank=True, null=True)
+    runtime = models.CharField(max_length=10, blank=True, null=True)
+    rating = models.CharField(max_length=50, blank=True, null=True)
+    showtime1 = models.TimeField(blank=True, null=True)
+    showtime2 = models.TimeField(blank=True, null=True)
+    showtime3 = models.TimeField(blank=True, null=True)
 
+    def __str__(self):
+        return self.title
