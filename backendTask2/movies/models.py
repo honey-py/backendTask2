93959f873fe1876658fd402f34b60b2d94ae7fc0
Harshit Jangir <it.s.me.of.em@gmail.com>
2024-09-21 18:59:33 +0530

    Initial commit

---
+++ backendTask2/movies/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+
